0c0a40491e3091bdfaef7587151b6d7b005e92e9
roberjglez <roberinfor94@gmail.com>
2025-01-26 02:29:08 +0100

    Adding kubernetes resources files

--- src/main/java/com/rjglez/order/controller/OrderController.java
+++ src/main/java/com/rjglez/order/controller/OrderController.java
@@ -3,6 +3,8 @@ package com.rjglez.order.controller;
 import com.rjglez.order.controller.request.OrderRequest;
 import com.rjglez.order.controller.response.OrderResponse;
 import com.rjglez.order.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,15 +16,18 @@ import java.util.UUID;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/orders")
+@Tag(name = "Order Service", description = "Endpoints for managing orders")
 public class OrderController {
 
     private final OrderService orderService;
 
+    @Operation(description = "Creates a new order for a given product and quantity. Checks inventory before processing")
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<OrderResponse> createOrder(@RequestBody OrderRequest orderRequest) {
         return ResponseEntity.ok(orderService.createOrder(orderRequest));
     }
 
+    @Operation(description = "Returns the information for a given order ID")
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<OrderResponse> getOrder(@PathVariable(value = "id") UUID id) {
         OrderResponse order = orderService.getOrder(id);
